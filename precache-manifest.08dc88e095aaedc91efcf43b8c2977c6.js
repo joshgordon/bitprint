69778a705b9d02446dc22902587c2c8e0d05282b
Josh Gordon <github@joshgordon.net>
2020-04-29 01:46:17 -0400

    Updates

---
+++ precache-manifest.08dc88e095aaedc91efcf43b8c2977c6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a0e802080ba6470b5e525816541a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2ccc432e853de25dd76",
+    "url": "/static/js/2.a1226985.chunk.js"
+  },
+  {
+    "revision": "cc7c4b5e108e02f94ce2",
+    "url": "/static/js/main.16089cb0.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "653d939e167fa3838aca2a584f019d61",
+    "url": "/static/media/arrow.653d939e.svg"
+  },
+  {
+    "revision": "52dd6a0d19a954232bd2d060c0f09eab",
+    "url": "/static/media/exclamation-circle.52dd6a0d.svg"
+  },
+  {
+    "revision": "3e6f5ba340111064aff4af76fb482b91",
+    "url": "/static/media/file-import.3e6f5ba3.svg"
+  },
+  {
+    "revision": "ce8f111fe0ef1ce3c42aac21b6896a01",
+    "url": "/static/media/frown-open.ce8f111f.svg"
+  },
+  {
+    "revision": "1bc90693f03f59a946e54d99a79a9cf9",
+    "url": "/static/media/print.1bc90693.svg"
+  },
+  {
+    "revision": "9b7b9251b5d5b4379a42ce2c42b9f330",
+    "url": "/static/media/step-1.9b7b9251.png"
+  },
+  {
+    "revision": "b8adb2d2948534401b743cd3b779a025",
+    "url": "/static/media/step-2.b8adb2d2.png"
+  },
+  {
+    "revision": "dda519a5208de8af782d121c961bce3f",
+    "url": "/static/media/step-3.dda519a5.png"
+  },
+  {
+    "revision": "a3e4c2f7c32ce9a7ad6fa8d381b9d9cf",
+    "url": "/static/media/trash.a3e4c2f7.svg"
+  }
+]);
